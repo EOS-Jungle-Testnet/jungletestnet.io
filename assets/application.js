6669bb718891cbcf4a11f9ef6c9f568952459831
Jose Andres Bogantes Ramirez <joseboga16@gmail.com>
2018-11-08 14:45:59 -0600

    Add form account migration (#34)
    * Add form account migration
    closes #16
    connect to #16
    
    * update credentials
    
    * update validation

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
   "use strict";
 
+  /******************** Set firebase config ********************/
+
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
+
+  firebase.initializeApp(config)
+
+  var dbRef = firebase.database().ref('accounts')
+
   /******************** NAVBAR ********************/
   var animationProp = $('.navbar-nemo'); //Navbar wraper
 
@@ -754,6 +769,34 @@ $(document).ready(function () {
   $('#nav, #nav2').on('click', function (e) {
     var currentID = $(this).attr('id');
     window.open($('#' + currentID).attr('href'))
+  })
+
+  $('#migrate-account-form').submit(function (e) {
+    e.preventDefault();
+    var accountName = $('#accountName').val();
+    var activeKey = $('#activeKey').val();
+    var ownerKey = $('#ownerKey').val();
+    var contractsCode = $('#contractsCode').prop('checked');
+
+    if (accountName.length === 12 && activeKey && ownerKey) {
+      var myDBRef = dbRef.push()
+      myDBRef.set({
+        account: accountName,
+        migrate_contracts: contractsCode,
+        active_key: activeKey,
+        owner_key: ownerKey
+      }, function (error) {
+        if (error) {
+          alert('Error saving the data')
+        } else {
+          $('#accountName').val('');
+          $('#activeKey').val('');
+          $('#ownerKey').val('');
+          $('#contractsCode').prop('checked', false);
+          alert('Saved Data')
+        }
+      })
+    }
   })
 
 });
